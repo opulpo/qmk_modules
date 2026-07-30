44b678c750511e61c2ba25d2bd710b9a5c748b77
Frann <pulpodesign@pm.me>
2026-07-30 20:58:03 +0200

    feat: add an option to use Super/Cmd rather than Alt

--- magic_layer_alt_tab/magic_layer_alt_tab.c
+++ magic_layer_alt_tab/magic_layer_alt_tab.c
@@ -18,32 +18,45 @@
 #    error "Magic Layer Alt-Tab requires MAGIC_LAYER_ALT_TAB_LAYER to be defined. Please set it the layer of your choice in your config.h"
 #endif
 
+// Windows uses Alt-tab, macOS Cmd-tab, and Linux desktops may use any or both.
+// By default, the modifier key used is Alt.
+// To use Super/Cmd instead of Alt, define MAGIC_LAYER_GUI_MOD in your config.h.
+#ifdef MAGIC_LAYER_GUI_MOD
+#   define MAGIC_MOD KC_LGUI
+#   define MAGIC_COMBO LGUI(KC_TAB)
+#   define MAGIC_COMBO_SHIFTED LSG(KC_TAB)
+#else
+#   define MAGIC_MOD KC_LALT
+#   define MAGIC_COMBO LALT(KC_TAB)
+#   define MAGIC_COMBO_SHIFTED LSA(KC_TAB)
+#endif
+
 static bool alt_tab_running = false;
 
 static void disable_magic_layer_alt_tab(void) {
-    unregister_mods(MOD_BIT(KC_LALT));
+    unregister_mods(MOD_BIT(MAGIC_MOD));
     alt_tab_running = false;
 }
 
 bool process_record_magic_layer_alt_tab(uint16_t keycode, keyrecord_t *record) {
-    const bool is_alt_tab_key = keycode == LALT(KC_TAB) || keycode == LSA(KC_TAB);
+    const bool is_alt_tab_key = keycode == MAGIC_COMBO || keycode == MAGIC_COMBO_SHIFTED;
 
     if (!is_alt_tab_key || !IS_LAYER_ON(MAGIC_LAYER_ALT_TAB_LAYER)) {
-        if (alt_tab_running && keycode != KC_BTN1) {
+        if (alt_tab_running && keycode != MS_BTN1) {
             disable_magic_layer_alt_tab();
         }
         return true;
     }
 
     if (!alt_tab_running) {
-        register_mods(MOD_BIT(KC_LALT));
+        register_mods(MOD_BIT(MAGIC_MOD));
         alt_tab_running = true;
     }
 
     if (record->event.pressed) {
-        if (keycode == LALT(KC_TAB)) {
+        if (keycode == MAGIC_COMBO) {
             tap_code16(KC_TAB);
-        } else if (keycode == LSA(KC_TAB)) {
+        } else if (keycode == MAGIC_COMBO_SHIFTED) {
             tap_code16(LSFT(KC_TAB));
         }
     }
